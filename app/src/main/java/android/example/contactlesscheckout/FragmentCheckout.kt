26096b452369e49b3253b4b985aaa83ae9f95eae
Haroun Hatmal <harounali2018@gmail.com>
2020-09-10 23:04:49 -0700

    first commit

---
+++ app/src/main/java/android/example/contactlesscheckout/FragmentCheckout.kt
@@ -0,0 +1,22 @@
+package android.example.contactlesscheckout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentCheckout : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_checkout, container, false)
+    }
+
+}
